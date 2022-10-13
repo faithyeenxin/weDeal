0e67e64c946f97de8b7a47c98b7ac5aa91c9d129
Faith Ye <faith.ye@hotmail.com>
2022-10-13 16:28:44 +0800

    update package.json index.js to server.js

---
+++ package-copy.js
@@ -0,0 +1,21 @@
+// {
+//   "name": "04_wedeal_final",
+//   "version": "1.0.0",
+//   "description": "",
+//   "main": "server.js",
+//   "dependencies": {
+//     "turbo-darwin-arm64": "^1.5.5"
+//   },
+//   "devDependencies": {
+//     "turbo": "^1.5.5"
+//   },
+//   "workspaces": [
+//     "packages/*",
+//     "apps/*"
+//   ],
+//   "scripts": {
+//     "test": "echo \"Error: no test specified\" && exit 1"
+//   },
+//   "author": "",
+//   "license": "ISC"
+// }
