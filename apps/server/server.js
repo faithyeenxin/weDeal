c2f63c2a258c5fb5b1aed1f47d9c35a20fc480bb
Faith Ye <faith.ye@hotmail.com>
2022-10-15 15:14:23 +0800

    path

--- apps/server/server.js
+++ apps/server/server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT ?? 3000;
 const cloudinary = require("cloudinary");
 const multer = require("multer");
-import * as path from "path";
+const path = require("path");
 
 //////////////////////////////////////////////////////
 //// * Controllers
