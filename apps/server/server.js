cb9864860c2e681309211ab94ba11e605e44517b
Faith Ye <faith.ye@hotmail.com>
2022-10-15 18:50:40 +0800

    changing client dist route

--- apps/server/server.js
+++ apps/server/server.js
@@ -93,7 +93,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/dist", "index.html"));
+  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
 });
 
 app.listen(port, () => {
