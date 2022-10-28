3bd3d768bc3f27e38edf0f60f8cd874e6e752e65
faithyeenx5 <faith.ye@hotmail.com>
2022-10-28 17:54:54 +0800

    Promise<boolean | void>

--- apps/client/src/pages/DealPageEdittable.tsx
+++ apps/client/src/pages/DealPageEdittable.tsx
@@ -103,7 +103,7 @@ const DealPageEdittable = () => {
         .test(
           "location-exist",
           "Location does not exist",
-           (location: any): boolean | void =>  {
+           (location: any): Promise<boolean | void> =>  {
             return axios.get(
                 `https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyDBh_veIl8kLIgp2gCyglYDvnl-d0EK9CU&address=${location}`
               )
