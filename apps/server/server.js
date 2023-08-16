9eba8c0a93cafe65264fa4fb52a4081731c01d0d
faithyeenx5 <faith.ye94@gmail.com>
2023-08-16 20:44:37 +0800

    feat(github actions + force https): added github actions + force https

--- apps/server/server.js
+++ apps/server/server.js
@@ -1,31 +1,33 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = process.env.PORT ?? 3500;
-const cloudinary = require("cloudinary");
-const multer = require("multer");
-const path = require("path");
+const cloudinary = require('cloudinary');
+const multer = require('multer');
+const path = require('path');
+const forceHttps = require('express-force-https');
 
 //////////////////////////////////////////////////////
 //// * Controllers
 //////////////////////////////////////////////////////
-const UserController = require("./controller/UserController");
-const DealController = require("./controller/DealController");
-const DealImagesController = require("./controller/DealImagesController");
-const CategoryController = require("./controller/CategoryController");
-const VoteController = require("./controller/VoteController");
+const UserController = require('./controller/UserController');
+const DealController = require('./controller/DealController');
+const DealImagesController = require('./controller/DealImagesController');
+const CategoryController = require('./controller/CategoryController');
+const VoteController = require('./controller/VoteController');
 
 //////////////////////////////////////////////////////
 //// * Middleware
 //////////////////////////////////////////////////////
-app.use(express.static("../client/dist"));
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(forceHttps);
+app.use(express.static('../client/dist'));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
-app.use("/api/user", UserController);
-app.use("/api/deal", DealController);
-app.use("/api/dealimages", DealImagesController);
-app.use("/api/category", CategoryController);
-app.use("/api/votes", VoteController);
+app.use('/api/user', UserController);
+app.use('/api/deal', DealController);
+app.use('/api/dealimages', DealImagesController);
+app.use('/api/category', CategoryController);
+app.use('/api/votes', VoteController);
 
 //////////////////////////////////////////////////////
 //// * Cloudinary
@@ -35,7 +37,7 @@ const { cloudinaryName, cloudinaryApiKey, cloudinaryApiSecret } = process.env;
 
 const storage = multer.diskStorage({
   filename: function (req, file, cb) {
-    cb(null, file.fieldname + "-" + Date.now());
+    cb(null, file.fieldname + '-' + Date.now());
   },
 });
 
@@ -48,9 +50,9 @@ cloudinary.config({
 const upload = multer({ storage });
 
 // get project images
-app.get("/api/getimages", async (req, res) => {
+app.get('/api/getimages', async (req, res) => {
   const { resources } = await cloudinary.v2.search
-    .expression("folder:projects")
+    .expression('folder:projects')
     .max_results(8)
     .execute();
 
@@ -60,18 +62,18 @@ app.get("/api/getimages", async (req, res) => {
 
 // upload project images
 app.post(
-  "/api/upload-images",
-  upload.array("uploadedFiles", 10),
+  '/api/upload-images',
+  upload.array('uploadedFiles', 10),
   async (req, res) => {
     try {
       const imagesFiles = req.files;
       console.log(imagesFiles);
       if (!imagesFiles)
-        return res.status(400).send({ msg: "No picture attached!" });
+        return res.status(400).send({ msg: 'No picture attached!' });
 
       const multiplePicturePromise = imagesFiles.map((picture) =>
         cloudinary.v2.uploader.upload(picture.path, {
-          upload_preset: "Project",
+          upload_preset: 'Project',
         })
       );
 
@@ -81,7 +83,7 @@ app.post(
 
       res.status(200).send({ imageLinks });
     } catch (err) {
-      res.status(500).send({ msg: "Unable to upload" });
+      res.status(500).send({ msg: 'Unable to upload' });
     }
   }
 );
@@ -90,12 +92,12 @@ app.post(
 //// * General Routes
 //////////////////////////////////////////////////////
 
-app.get("/", (req, res) => {
-  res.send("This is my backend home route!");
+app.get('/', (req, res) => {
+  res.send('This is my backend home route!');
 });
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
 });
 
 app.listen(port, () => {
